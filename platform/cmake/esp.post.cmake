7f6a1753cb484ce88731a4af93a921c3ca8c23ee
Giel Willemsen <33036522+GWillemsen@users.noreply.github.com>
2025-07-17 19:32:02 +0000

    Remove unused cmake alias ESP-IDF
    Don't make the alias for the IDF component in the 'regular' cmake
    namespace. If working with ESP-IDF, then all packages should support
    ESP-IDF, and we don't create the names that the generic platform
    creates. Otherwise, what is the point of the ESP-IDF specific platform.

--- platform/cmake/esp.post.cmake
+++ platform/cmake/esp.post.cmake
@@ -8,9 +8,6 @@ set(LIBOS_SRCS
 idf_component_register(SRCS ${LIBOS_SRCS}
                        INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/include")
 
-# Just in case plain cmake is used somewhere else, give it an alias for the 'regular' name.
-add_library(libOS ALIAS ${COMPONENT_LIB})
-
 if (NOT COMMAND convert_config_to_target)
     function(convert_config_to_target var_name)
         if ("${CONFIG_${var_name}}" STREQUAL "y")
